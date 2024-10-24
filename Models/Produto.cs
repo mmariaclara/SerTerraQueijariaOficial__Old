bc3a25f5dc7030339d7757eb244ac82a4903b1c5
clara <clarinhapereira002@gmail.com>
2024-10-24 15:37:00 -0300

    Mudando o Layout das Páginas do Admin

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace SerTerraQueijaria.Models
 {
@@ -7,20 +8,24 @@ namespace SerTerraQueijaria.Models
 
         public Guid ProdutoId { get; set; }
 
-        [DisplayName("Produto")]
+        [DisplayName("Nome do Produto")]
+        [Required(ErrorMessage = "É necessário ter um Nome do Produto.")]
         public string NomeProduto { get; set; }
 
         [DisplayName("Descrição")]
+        [Required(ErrorMessage = "É necessário ter uma Descrição.")]
         public string Descricao { get; set; }
 
         [DisplayName("Preço")]
+        [Required(ErrorMessage = "É necessário ter um Preço.")]
         public decimal PrecoUnitario { get; set; }
 
         [DisplayName("Estoque")]
+        [Required(ErrorMessage = "É necessário ter um Estoque.")]
         public int QtdEstoque { get; set; }
 
         [DisplayName("Categoria")]
-
+        [Required(ErrorMessage = "É necessário ter uma Categoria.")]
         public Guid TipoProdutoId { get; set; }
 
         public TiposProdutos TipoProd {  get; set; }
