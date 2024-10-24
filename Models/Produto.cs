6a0bd73f7577cf076c43cbe3dcc4353c76bbf470
clara <clarinhapereira002@gmail.com>
2024-10-24 14:35:14 -0300

    Alterações no layout das páginas do Admin

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,13 +1,28 @@
-﻿namespace SerTerraQueijaria.Models
+﻿using System.ComponentModel;
+
+namespace SerTerraQueijaria.Models
 {
     public class Produto
     {
+
         public Guid ProdutoId { get; set; }
+
+        [DisplayName("Produto")]
         public string NomeProduto { get; set; }
+
+        [DisplayName("Descrição")]
         public string Descricao { get; set; }
+
+        [DisplayName("Preço")]
         public decimal PrecoUnitario { get; set; }
+
+        [DisplayName("Estoque")]
         public int QtdEstoque { get; set; }
+
+        [DisplayName("Categoria")]
+
         public Guid TipoProdutoId { get; set; }
+
         public TiposProdutos TipoProd {  get; set; }
     }
 }
